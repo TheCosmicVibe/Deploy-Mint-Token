6e6f0adab2b30bceec08a6840e45f0133333d7de
TheCosmicVibe <157511445+TheCosmicVibe@users.noreply.github.com>
2025-11-11 22:52:25 +0100

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+require('dotenv').config();
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying with:", deployer.address);
+
+  const MyToken = await hre.ethers.getContractFactory("MyToken");
+  // name, symbol, initial supply (use parseUnits for decimals)
+  const initialSupply = hre.ethers.utils.parseUnits("1000", 18); // 1000 tokens
+  const token = await MyToken.deploy("MyToken", "MTK", initialSupply);
+  await token.deployed();
+
+  console.log("MyToken deployed to:", token.address);
+  console.log("Verify with: npx hardhat verify --network <network> <address> \"MyToken\" \"MTK\" \"1000000000000000000000\"");
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
