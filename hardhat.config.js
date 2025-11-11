ae64fb8e250c417d9eb1d15ba97c33728ce89236
TheCosmicVibe <157511445+TheCosmicVibe@users.noreply.github.com>
2025-11-11 22:23:19 +0100

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { RPC_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: RPC_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+    }
+  }
+};
