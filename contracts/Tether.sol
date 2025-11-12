2c2e885ef6fe0fc352b4eb9542bb691f238fe014
TheCosmicVibe <157511445+TheCosmicVibe@users.noreply.github.com>
2025-11-12 06:29:07 +0100

    Update Tether.sol

--- contracts/Tether.sol
+++ contracts/Tether.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
+contract Tether is ERC20, Ownable {
     constructor(string memory name_, string memory symbol_, uint256 initialSupply) ERC20(name_, symbol_) {
         if (initialSupply > 0) {
             _mint(msg.sender, initialSupply);
